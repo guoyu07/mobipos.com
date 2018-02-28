a8661bd4b231cde2cb391b940b090c85ae06d57a
royWati <royywati@gmail.com>
2018-02-28 16:55:24 +0300

    master
    Co-Authored-By: ChrisCurtis93 <chriscurtis93@users.noreply.github.com>

---
+++ app/cashier-get-discounts.php
@@ -0,0 +1,49 @@
+<?php
+
+include('controllers/db.php');
+
+$response=array();
+
+global $db;
+
+if(isset($_REQUEST['user_id'])){
+
+		$user_id=$_REQUEST['user_id'];
+
+    $client_id=$db->GetOne("SELECT client_id FROM tb_employees WHERE id=$user_id");
+
+			$discounts=$db->GetArray("SELECT * FROM tb_discounts where client_id=$client_id AND active_status
+				=1");
+			$response['data']=array();
+
+
+				foreach($discounts as $row) {
+				$data=array();
+
+        $data["id"]=$row["id"];
+				$data["discount_name"]=$row["discount name"];
+				$data["discount_value"]=$row["discount_value"];
+
+				array_push($response['data'], $data);
+
+
+
+			}
+
+
+			$response['success']=1;
+			$response['message']='categories pulled';
+
+			echo json_encode($response);
+
+
+
+
+}else{
+	$response['success']=0;
+	$response['message']='missing user id';
+
+	echo json_encode($response);
+}
+
+?>
